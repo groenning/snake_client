aa22ebc103a661fe6284dc3e43e04da8227c1481
VictorHolving <vholving@gmail.com>
2015-10-15 09:58:59 +0200

    Kommentar ændret

--- src/client.java
+++ src/client.java
@@ -1,7 +1,7 @@
 /**
  * Created by Tobias on 15/10/15.
  */
-// prototype på kliens som taler med serveren
+// prototype på klient som taler med serveren
 public class client {
 
 public static void main (String []args){
