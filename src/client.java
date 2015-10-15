fde96ed9fd6ef28395c33d90e035ff7ac3b09d45
Tobias Jacobsen <tobias_jac@hotmail.com>
2015-10-15 10:30:29 +0200

    Ny .gitignore fil

--- src/client.java
+++ src/client.java
@@ -1,5 +1,7 @@
+import java.lang.System;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.*;
 
 /**
  * Created by Tobias on 15/10/15.
@@ -10,7 +12,7 @@ public class client {
 
     public static void main (String []args){
 
-
+        System.out.println("BLA");
         // test connection
         // lad os bare bruge det fra øvelsestimen
         // ny kommentar
