1b05519b0982cc441ee9ac6dc706c3292720e922
VictorHolving <vholving@gmail.com>
2015-10-15 10:06:24 +0200

    import af java socket

--- src/client.java
+++ src/client.java
@@ -2,6 +2,10 @@
  * Created by Tobias on 15/10/15.
  */
 // prototype på klient som taler med serveren
+
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class client {
 
 public static void main (String []args){
