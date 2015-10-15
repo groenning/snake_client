73e2165e4fab2eb6f322105f1b29ac9be21e4038
tobiaslundjeremiassen <tobiasjeremiassen94@gmail.com>
2015-10-15 10:36:32 +0200

    test code

--- src/client.java
+++ src/client.java
@@ -1,13 +1,30 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by Tobias on 15/10/15.
  */
-public class client {
+public class Client {
 
     private String hostname;
     private int port;
     Socket socketClient;
 
-    
+        //Skriv en kommentar (konstruktør)
+        public Client (String hostname, int port){
+            this.hostname = hostname;
+            this.port = port;
+
+        }
+    // UnknownHostException
+
+    public void connection() throws UnknownHostException, IOException{
+
+        SocketClient = new Socket(hostname,port);
+    }
+
+
     public static void main(String[] args) {
 
         //This line create a socketclient object
