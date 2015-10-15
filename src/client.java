a7c6f628ec3b7ccdfb597f265aea15fb9ad02529
tobiaslundjeremiassen <tobiasjeremiassen94@gmail.com>
2015-10-15 10:26:21 +0200

    test code

--- src/client.java
+++ src/client.java
@@ -1,44 +1,18 @@
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-
 /**
  * Created by Tobias on 15/10/15.
  */
 public class client {
 
-public static void main (String []args){
+    private String hostname;
+    private int port;
+    Socket socketClient;
 
-    try {
+    
+    public static void main(String[] args) {
 
-        Client client = Client.create();
-
-        WebResource webResource = client.resource("");
-
-
-        ClientResponse response = webResource.accept("application/json")
-                .get(ClientResponse.class);
-
-        if (response.getStatus() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + response.getStatus());
-        }
-
-
-        String output = response.getEntity(String.class);
-
-        System.out.println("Output from Server .... \n");
-        System.out.println(output);
-
-    } catch (Exception e) {
-
-        e.printStackTrace();
-
+        //This line create a socketclient object
+        SocketClient client = new SocketClient ("Localhost",12312);
     }
 
-
-    //Tobias er en svans
-
 }
- }
 
